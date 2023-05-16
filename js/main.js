7419fd7bd703ec904f4c97e9ce330670486b151c
Federico Vittorini <federicovittorini@gmail.com>
2023-05-16 18:49:45 -0300

    Clase 13

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 import * as Popper from "/node_modules/@popperjs/core/dist/umd/popper.js"
 import * as bootstrap from '/node_modules/bootstrap/dist/js/bootstrap.js'
 
+// delta para un exponential backoff
+var cadenaTexto = "T"; // tipo String
+var num = 6; // tipo Number
+var nn = cadenaTexto + num;
+
+
+var a = null;
+alert(a);
+/**
+ * gfwegdsfsd
+ */
